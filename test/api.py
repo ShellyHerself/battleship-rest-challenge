8b0d3df4f47ba2419e7a37ea08c4653dc6b68e8f
gbMichelle <gbmichelle.dev@gmail.com>
2021-04-11 17:16:34 +0200

    Test moves

--- test/api.py
+++ test/api.py
@@ -3,6 +3,7 @@ from pocha import before, beforeEach, describe, it
 from hamcrest import *
 
 import main
+from game_logic import BattleShipGame
 from db import wipeDatabase, DbBattleShipGame
 
 from main import app as server_app
@@ -100,3 +101,48 @@ def apiTests():
         assert_that(new_game['status'], equal_to("IN_PROGRESS"), "Game has started because we joined two players")
         assert_that(new_game['player_count'], equal_to(2), "There's two players after we joined")
         assert_that(new_game['turn_player'], equal_to(0), "No turn selected yet because we're pregame")
+
+    @it("Players can make moves")
+    def canMakeMoves():
+        # Create a game with two players in the database
+        game = DbBattleShipGame.create()
+        game_obj = BattleShipGame()
+        game_obj.joinPlayer()
+        game_obj.joinPlayer()
+        game_obj.startIfEnoughPlayers()
+        # serialize the data from our game object back to the database
+        game.fromObject(game_obj)
+        game.save()
+
+        game_id = game.id
+
+        res = app.get('/games/{}'.format(game_id))
+        assert_that(res.status_code, equal_to(200), 'Can retrieve game')
+        listed_game = responseJsonToPy(res)
+
+        assert_that(listed_game['status'], equal_to("IN_PROGRESS"))
+
+        # We do this janky json.dumps in here because this test function
+        # apparently does not properly take in nested objects and will convert them
+        # to strings which causes single ' rather than " which can't be parsed
+        move1 = {"x":0, "y":0}
+        res = app.post('/games/{}/fire'.format(game_id), json={"player_id": 0, 'move': (json.dumps(move1))})
+        assert_that(res.status_code, equal_to(200), 'Can retrieve game')
+        move_response = responseJsonToPy(res)
+        # We fired at a location we know is a ship
+        assert_that(move_response['hit'], equal_to(True))
+
+        move2 = {"x":0, "y":1}
+        res = app.post('/games/{}/fire'.format(game_id), json={"player_id": 1, 'move': (json.dumps(move2))})
+        assert_that(res.status_code, equal_to(200), 'Can retrieve game')
+        move_response = responseJsonToPy(res)
+        # We fired at a location we know there isn't a ship
+        assert_that(move_response['hit'], equal_to(False))
+
+        # Check if the last moves are properly updated
+        res = app.get('/games/{}'.format(game_id))
+        assert_that(res.status_code, equal_to(200), 'Can retrieve game')
+        listed_game = responseJsonToPy(res)
+
+        assert_that(listed_game['player_last_moves'][0], equal_to(move1))
+        assert_that(listed_game['player_last_moves'][1], equal_to(move2))
